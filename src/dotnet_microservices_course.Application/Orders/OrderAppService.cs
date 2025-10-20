417c7c42a7b3adb0a25f06ab3aae7d591efbd404
Ahmad Almunajjed <ahmadalmunajjed@outlook.com>
2025-10-20 12:38:01 +0800

    update quantity in create order

--- src/dotnet_microservices_course.Application/Orders/OrderAppService.cs
+++ src/dotnet_microservices_course.Application/Orders/OrderAppService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using dotnet_microservices_course.Products;
+using System;
 using System.Threading.Tasks;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
@@ -13,8 +14,12 @@ public class OrderAppService : CrudAppService<
     PagedAndSortedResultRequestDto,
     CreateUpdateOrderDto>, IOrderAppService
 {
-    public OrderAppService(IRepository<Order, Guid> repository) : base(repository)
+    private readonly IRepository<Product, Guid> _productRepo;
+    public OrderAppService(
+        IRepository<Order, Guid> repository,
+        IRepository<Product, Guid> productRepor) : base(repository)
     {
+        _productRepo = productRepor;
     }
 
     public override async Task<OrderDto> CreateAsync(CreateUpdateOrderDto input)
@@ -38,6 +43,9 @@ public class OrderAppService : CrudAppService<
                 Quantity = item.Quantity.GetValueOrDefault(),
                 Price = item.Price.GetValueOrDefault()
             });
+            var product = await _productRepo.GetAsync(item.ProductId.GetValueOrDefault());
+            product.Quantity -= item.Quantity.GetValueOrDefault();
+            await _productRepo.UpdateAsync(product);
         }
         return order;
     }
